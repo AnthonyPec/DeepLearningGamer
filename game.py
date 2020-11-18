b8fbe8d2ef175a5bbe5cd2e44220d76d90634267
AnthonyPec <38145775+AnthonyPec@users.noreply.github.com>
2020-11-17 19:11:12 -0500

    breaking game up into more functions and created skeleton code for player class.

--- game.py
+++ game.py
@@ -5,40 +5,75 @@ import pygame
 import os
 from pygame import mixer
 
+
+class game:
+    def __init__(self):
+        self.bg = pygame.image.load("img/background.png")
+        self.crash = False
+        self.score = 0
+        self.cur_enemies = 1
+        self.gametime = 0
+
+
 class player:
-    def do_move(self, move, x, y, game, food, agent):
+
+    def __init__(self, game):
+        self.x = 0
+
+    def do_move(self, move, x, game, food, agent):
         move_array = [self.x_change, self.y_change]
 
-        if self.eaten:
-            self.position.append([self.x, self.y])
-            self.eaten = False
-            self.food = self.food + 1
-            #do nothing
-        if np.array_equal(move, [1, 0, 0]):
-            move_array = self.x_change, self.y_change
+        # do nothing
+        if np.array_equal(move, [1, 0, 0, 0, 0]):
+            move_array = 0
             # move right
-        elif np.array_equal(move, [0, 1, 0]) and self.y_change == 0:  # right - going horizontal
-            move_array = [0, self.x_change]
-            #move left
-        elif np.array_equal(move, [0, 1, 0]) and self.x_change == 0:  # right - going vertical
-            move_array = [-self.y_change, 0]
-            #sprint right
-        elif np.array_equal(move, [0, 0, 1]) and self.y_change == 0:  # left - going horizontal
-            move_array = [0, -self.x_change]
+        elif np.array_equal(move, [0, 1, 0, 0, 0]) and self.y_change == 0:
+            move_array = 1
+            # move left
+        elif np.array_equal(move, [0, 0, 1, 0, 0]) and self.x_change == 0:  # right - going vertical
+            move_array = -1
+            # sprint right
+        elif np.array_equal(move, [0, 0, 0, 1, 0]) and self.y_change == 0:  # left - going horizontal
+            move_array = 2
             # spring left
-        elif np.array_equal(move, [0, 0, 1]) and self.x_change == 0:  # left - going vertical
-            move_array = [self.y_change, 0]
-        self.x_change, self.y_change = move_array
-        self.x = x + self.x_change
-        self.y = y + self.y_change
+        elif np.array_equal(move, [0, 0, 0, 0, 1]) and self.x_change == 0:  # left - going vertical
+            move_array = -2
 
+        self.x = x + 7 * move
+
+        gametime = game.gametime + 1
+        if gametime % 460 == 0 and game.cur_enemies < num_of_enemies:
+            game.cur_enemies = game.cur_enemies + 1
+        for i in range(cur_enemies):
+
+            # Ball movement
+            enemyX[i] = enemyX[i] + enemyXVel[i]
+            enemyY[i] = enemyY[i] + enemyYVel[i]
+            enemyYVel[i] = enemyYVel[i] + 0.25
+
+            if enemyY[i] > yMax - 128:
+                enemyYVel[i] = -enemyYVel[i] * 0.85
+                enemyY[i] = yMax - 128
+            if enemyX[i] > xMax or enemyX[i] < -128:
+                enemyY[i] = random.randint(0, 200)
+                enemyYVel[i] = 5
+                enemyXVel[i] = -enemyXVel[i]
+                score_value = score_value + 1
+            enemy(enemyX[i], enemyY[i], i)
+
+            # Collision
+            collision = isCollision(enemyX[i] + 64, enemyY[i] + 64, playerX + 85, playerY + 120)
+            if collision:
+                game_over = True
+                running = False
+        # fix this game crash not correct
+        """
         if self.x < 20 or self.x > game.game_width - 40 \
                 or self.y < 20 \
                 or self.y > game.game_height - 40 \
                 or [self.x, self.y] in self.position:
             game.crash = True
-
-
+        """
 
 
 # Initialize the pygame
@@ -57,7 +92,6 @@ screen = pygame.display.set_mode((xMax, yMax))
 
 # Background
 background = pygame.image.load('background.jpg')
-
 
 # Sound
 mixer.music.load("darude.wav")
@@ -98,7 +132,6 @@ for i in range(num_of_enemies):
     enemyXVel.append(random.randint(4, 10))
     enemyYVel.append(5)
 
-
 # Score
 
 score_value = 0
@@ -115,15 +148,19 @@ def show_score(x, y):
     score = font.render("Score : " + str(score_value), True, (0, 0, 0))
     screen.blit(score, (x, y))
 
+
 def game_over_text():
     over_text = over_font.render("GAME OVER", True, (0, 0, 0))
     screen.blit(over_text, (200, 250))
 
+
 def player(playerImg, x, y):
     screen.blit(playerImg, (x, y))
 
+
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
+
 
 def isCollision(enemyX, enemyY, playerX, playerY):
     distance = math.sqrt(math.pow(enemyX - playerX, 2) + (math.pow(enemyY - playerY, 2)))
@@ -132,95 +169,98 @@ def isCollision(enemyX, enemyY, playerX, playerY):
     else:
         return False
 
+
 # Game Loop
 running = True
 game_over = False
 sprint = False
-while running:
 
-    # RGB = Red, Green, Blue
-    screen.fill((0, 0, 0))
-    # Background Image
-    screen.blit(background, (0, 0))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def define_parameters():
+    params = dict()
+    # Neural Network
+    params['epsilon_decay_linear'] = 1 / 75
+    params['learning_rate'] = 0.0005
+    params['first_layer_size'] = 50  # neurons in the first layer
+    params['second_layer_size'] = 300  # neurons in the second layer
+    params['third_layer_size'] = 50  # neurons in the third layer
+    params['episodes'] = 150
+    params['memory_size'] = 2500
+    params['batch_size'] = 1000
+    # Settings
+    params['weights_path'] = 'weights/weights3.hdf5'
+    params['load_weights'] = False
+    params['train'] = True
+    params['plot_score'] = True
+    params['computer_player'] = True
+    return params
 
-        # if keystroke is pressed check whether its right or left
-        keys = pygame.key.get_pressed()
-        playerX_change = 0
-        if keys[pygame.K_LSHIFT]:
-            sprint = True
-        else:
-            sprint = False
-        if keys[pygame.K_LEFT]:
-            playerX_change = -7 - int(sprint) * 7
-            curImage = playerImgL
-        if keys[pygame.K_RIGHT]:
-            playerX_change = 7 + int(sprint) * 7
-            curImage = playerImgR
 
-        """if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -7 - int(sprint) * 7
-                curImage = playerImgL
-            if event.key == pygame.K_RIGHT:
-                playerX_change =  7 + int(sprint) * 7
-                curImage = playerImgR
-            if event.key == pygame.K_LSHIFT:
-                sprint = True
+def run(param):
+    while running:
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        # RGB = Red, Green, Blue
+        screen.fill((0, 0, 0))
+        # Background Image
+        screen.blit(background, (0, 0))
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if param['computer_player']:
+                player.do_move()
+            else:
+                # if keystroke is pressed check whether its right or left
+                keys = pygame.key.get_pressed()
                 playerX_change = 0
-            if event.key == pygame.K_LSHIFT:
-                sprint = False """
+                if keys[pygame.K_LSHIFT]:
+                    sprint = True
+                else:
+                    sprint = False
+                if keys[pygame.K_LEFT]:
+                    playerX_change = -7 - int(sprint) * 7
+                    curImage = playerImgL
+                if keys[pygame.K_RIGHT]:
+                    playerX_change = 7 + int(sprint) * 7
+                    curImage = playerImgR
 
-    playerX += playerX_change
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= xMax - playerSizeX:
-        playerX = xMax - playerSizeX
+            """if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -7 - int(sprint) * 7
+                    curImage = playerImgL
+                if event.key == pygame.K_RIGHT:
+                    playerX_change =  7 + int(sprint) * 7
+                    curImage = playerImgR
+                if event.key == pygame.K_LSHIFT:
+                    sprint = True
+    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+                if event.key == pygame.K_LSHIFT:
+                    sprint = False """
 
-    # Enemy Movement
-    gametime = gametime + 1
-    if gametime % 460 == 0 and cur_enemies < num_of_enemies:
-        cur_enemies = cur_enemies + 1
-    for i in range(cur_enemies):
+        playerX += playerX_change
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= xMax - playerSizeX:
+            playerX = xMax - playerSizeX
 
-        #Ball movement
-        enemyX[i] = enemyX[i] + enemyXVel[i]
-        enemyY[i] = enemyY[i] + enemyYVel[i]
-        enemyYVel[i] = enemyYVel[i] + 0.25
+        # Enemy Movement
 
-        if enemyY[i] > yMax-128:
-            enemyYVel[i] = -enemyYVel[i] * 0.85
-            enemyY[i] = yMax-128
-        if enemyX[i] > xMax or enemyX[i] < -128:
-            enemyY[i] = random.randint(0, 200)
-            enemyYVel[i] = 5
-            enemyXVel[i] = -enemyXVel[i]
-            score_value = score_value + 1
-        enemy(enemyX[i], enemyY[i], i)
+        player(curImage, playerX, playerY)
+        show_score(textX, textY)
+        pygame.display.update()
+        fpsClock.tick(FPS)
 
-        # Collision
-        collision = isCollision(enemyX[i]+64, enemyY[i]+64, playerX+85, playerY+120)
-        if collision:
-            game_over = True
-            running = False
-
-    player(curImage, playerX, playerY)
-    show_score(textX, textY)
-    pygame.display.update()
-    fpsClock.tick(FPS)
-
-while(game_over):
-    # RGB = Red, Green, Blue
-    screen.fill((0, 0, 0))
-    # Background Image
-    screen.blit(background, (0, 0))
-    show_score(textX, textY)
-    game_over_text()
-    pygame.display.update()
-    fpsClock.tick(FPS)
+    while (game_over):
+        # RGB = Red, Green, Blue
+        screen.fill((0, 0, 0))
+        # Background Image
+        screen.blit(background, (0, 0))
+        show_score(textX, textY)
+        game_over_text()
+        pygame.display.update()
+        fpsClock.tick(FPS)
